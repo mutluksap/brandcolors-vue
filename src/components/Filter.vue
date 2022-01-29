09d79632d1a6220cbc24d5e6e6a32f63a24481c1
mutluksap <mtlksp@gmail.com>
2022-01-30 02:17:40 +0300

    blob add project

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -7,11 +7,6 @@
         <div class="collections">
             <div class="download-collections">
             <ul :class="{active : getSelectedColors.length > 0}">
-                <li>
-                    <a href="#">
-                        <i class="fas fa-download"></i>
-                    </a>
-                </li>
                 <li @click="showSelectedBrands">
                     <a href="#">
                         <i class="fas fa-link"></i>
@@ -28,7 +23,7 @@
             </ul>
             
         </div>
-        <a class="download-colors" href="#">
+        <a download="asd.jpg" @click="downloadColors" class="download-colors" href="#">
                 <i class="fas fa-download"></i>
                 <span>All Brands</span>
         </a>
@@ -39,6 +34,7 @@
 
 <script>
 import {mapGetters} from "vuex"
+import axios from "axios"
 export default {
     methods: {
         getWords(e){
@@ -49,6 +45,21 @@ export default {
         },
         showSelectedBrands(){
             prompt("Here's the URL to share", "https://localhost:8080/"+ this.$store.getters.getSelectedColorsSlug.join());
+        },
+        downloadColors(){
+             axios({
+                    url: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Teemo_18.jpg', // File URL Goes Here
+                    method: 'GET',
+                    responseType: 'blob',
+                }).then((res) => {
+                    console.log(res);
+                     var FILE = window.URL.createObjectURL(new Blob([res.data]));
+                     var docUrl = document.createElement('x');
+                     docUrl.href = FILE;
+                     docUrl.setAttribute('download', 'asd.jpg');
+                     document.body.appendChild(docUrl);
+                     docUrl.click();
+                });
         }
     },
     computed: {
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,5 @@
-// module.exports = {
-//   proxy: {
-//     "/V1": {
-//       target: "http://localhost:8888",
-//       changeOrigin: true,
-//       pathRewrite: {
-//         "^/V1": "",
-//       },
-//     },
-//     "/V2": {
-//       target: "https://loclhost:4437",
-//       changeOrigin: true,
-//       pathRewrite: {
-//         "^/V2": "",
-//       },
-//     },
-//   },
-// };
+module.exports = {
+  devServer: {
+    port: 8080,
+  },
+};
